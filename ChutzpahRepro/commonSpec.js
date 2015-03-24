1b9efdde65d72ad7a817ad19c38cb47df0655127
GoeniGoeGoe <michaelvrooijen@gmail.com>
2015-03-24 16:40:45 +0100

    Added repro for Chutzpah coverage tracing issue

---
+++ ChutzpahRepro/commonSpec.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="common.js" /> the error is only thrown when: 1. a reference is included in the spec file (maybe also in a library) 2. tracing is enabled 3. coverage is enabled
+
+describe("test", function () {
+
+    it("can be tested", function () {
+
+        var viewModel = true;
+        expect(viewModel).toBe(true);
+    });
+});
